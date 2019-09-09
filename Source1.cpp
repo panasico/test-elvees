2a39f4d873e230a78faa219daa60278b2e7c7ad9
panasico <chrome266@gmail.com>
2019-09-09 10:17:26 +0300

    rename

--- Source1.cpp
+++ Source1.cpp
@@ -4,60 +4,117 @@
 #include <ctime>
 using namespace std;
 
-int function(int N) {
-	if (N == 0)
-		return 0;
-	if (N == 1)
-		return 1;
+int function(int size, int * arr) {
+	int res = 0;
 
-	if (N % 2 == 0)
-		return function(N / 2);
-	else
-		return function(N - 1) + function(N / 2 + 1);
+	for (int i = 0; i < size; i++)
+		res ^= arr[i];
+
+	return res;
 }
 
 int main(int argc, char* argv[])
 {
-	int N;
-
-	if (argc == 2) {
-		//if rand-mode
-		if (strcmp(argv[1], "rand") == 0) {
-			srand(time(0));
-			N = rand();
-			cout << "Result : " << function(N);
-			return 0;
-		}
-		//if N from input arguments
-		try {
-			N = atoi(argv[1]);
-		}
-		catch (...) {
-			cout << "error! program works with integer argument!" << endl;
-			return -1;
+	int size = 0; //size of array
+	int flag = 0; //flag input arguments
+	int* arr; 
+	
+	//rand-mode
+	if (argc == 3 && (strcmp(argv[1],"rand") == 0 || strcmp(argv[2], "rand") == 0)) {
+		if (strcmp(argv[1], "rand") == 0)
+			size = atoi(argv[2]);
+		else
+			size = atoi(argv[1]);
+		if (size % 2 == 0) {
+			cout << "error! program works with an odd array size!" << endl;
+			return -2;
 		}
 
-
-		if (N >= 0) {
-			cout << "Result : " << function(N);
-			return 0;
+		flag = 1;
 		}
-		else {
-			cout << "error! program works with integer argument!" << endl;
-			return -1;
-		}
-	}
-
 	//cin-mode
-	cout << "Enter N : ";
-	try {
-		cin >> N;
-	}
-	catch (...) {
-		cout << "error! program works with integer argument!" << endl;
-		return -1;
-	}
+	if (argc == 1)
+		flag = 2;
 
-	cout << "Result : " << function(N);
+	switch (flag)
+	{
+		//elements of array from argv
+		case 0: {
+			size = argc - 1;
+
+			arr = new int[size];
+
+			for (int i = 1; i < argc; i++) {
+				try {
+					arr[i - 1] = atoi(argv[i]);
+				}
+				catch (...) {
+					cout << "error! program works with integer argument!" << endl;
+					return -1;
+				}
+			}
+			cout << "Result : " << function(size, arr) << endl;
+			break;
+		}
+
+		//cin-mode
+		case 2: {
+			cout << "Enter size of array : ";
+			try {
+				cin >> size;
+			}
+			catch (...) {
+				cout << "error! program works with integer argument!" << endl;
+				return -1;
+			}
+		
+			arr = new int[size];
+
+			cout << "Enter elements (through enter) : " << endl;
+			for (int i = 0; i < size; i++) {
+				cout << "1 element = ";
+				cin >> arr[i];
+			}
+
+			cout << "Result : " << function(size, arr) << endl;
+			break;
+		}
+
+		//rand-mode
+		case 1: {
+			srand(time(0));
+			arr = new int[size]();
+			int position = rand() % (size - 1); //position of searching element
+
+			for (int i = 0; i < size; i++) {
+				if (i == position) {
+					if (i == size - 1 || i % 2 == 0) {
+						arr[i] = rand() + 1;
+						continue;
+					}
+					arr[i + 1] = arr[i];
+					arr[i] = rand() + 1;
+					i++;
+					continue;
+				}
+				if (arr[i] != 0 && i > 0)
+					continue;
+				arr[i] = rand() + 1;
+				arr[i + 1] = arr[i];
+			}
+			
+			//print generated array
+			cout << "Generated array :" << endl;
+			for (int i = 0; i < size; i++) {
+				cout << arr[i] << " ";
+				if (i % 6 == 0 && i)
+					cout << endl;
+			}
+
+			cout << endl << endl << "Result : " << function(size, arr) << endl;
+			break;
+		}
+	}
+	delete[] arr;
 	return 0;
 }
--- Source2.cpp
+++ Source2.cpp
@@ -4,117 +4,60 @@
 #include <ctime>
 using namespace std;
 
-int function(int size, int * arr) {
-	int res = 0;
+int function(int N) {
+	if (N == 0)
+		return 0;
+	if (N == 1)
+		return 1;
 
-	for (int i = 0; i < size; i++)
-		res ^= arr[i];
-
-	return res;
+	if (N % 2 == 0)
+		return function(N / 2);
+	else
+		return function(N - 1) + function(N / 2 + 1);
 }
 
 int main(int argc, char* argv[])
 {
-	int size = 0; //size of array
-	int flag = 0; //flag input arguments
-	int* arr; 
-	
-	//rand-mode
-	if (argc == 3 && (strcmp(argv[1],"rand") == 0 || strcmp(argv[2], "rand") == 0)) {
-		if (strcmp(argv[1], "rand") == 0)
-			size = atoi(argv[2]);
-		else
-			size = atoi(argv[1]);
-		if (size % 2 == 0) {
-			cout << "error! program works with an odd array size!" << endl;
-			return -2;
-		}
+	int N;
 
-		flag = 1;
-		}
-	//cin-mode
-	if (argc == 1)
-		flag = 2;
-
-	switch (flag)
-	{
-		//elements of array from argv
-		case 0: {
-			size = argc - 1;
-
-			arr = new int[size];
-
-			for (int i = 1; i < argc; i++) {
-				try {
-					arr[i - 1] = atoi(argv[i]);
-				}
-				catch (...) {
-					cout << "error! program works with integer argument!" << endl;
-					return -1;
-				}
-			}
-			cout << "Result : " << function(size, arr) << endl;
-			break;
-		}
-
-		//cin-mode
-		case 2: {
-			cout << "Enter size of array : ";
-			try {
-				cin >> size;
-			}
-			catch (...) {
-				cout << "error! program works with integer argument!" << endl;
-				return -1;
-			}
-		
-			arr = new int[size];
-
-			cout << "Enter elements (through enter) : " << endl;
-			for (int i = 0; i < size; i++) {
-				cout << "1 element = ";
-				cin >> arr[i];
-			}
-
-			cout << "Result : " << function(size, arr) << endl;
-			break;
-		}
-
-		//rand-mode
-		case 1: {
+	if (argc == 2) {
+		//if rand-mode
+		if (strcmp(argv[1], "rand") == 0) {
 			srand(time(0));
-			arr = new int[size]();
-			int position = rand() % (size - 1); //position of searching element
+			N = rand();
+			cout << "Result : " << function(N);
+			return 0;
+		}
+		//if N from input arguments
+		try {
+			N = atoi(argv[1]);
+		}
+		catch (...) {
+			cout << "error! program works with integer argument!" << endl;
+			return -1;
+		}
 
-			for (int i = 0; i < size; i++) {
-				if (i == position) {
-					if (i == size - 1 || i % 2 == 0) {
-						arr[i] = rand() + 1;
-						continue;
-					}
-					arr[i + 1] = arr[i];
-					arr[i] = rand() + 1;
-					i++;
-					continue;
-				}
-				if (arr[i] != 0 && i > 0)
-					continue;
-				arr[i] = rand() + 1;
-				arr[i + 1] = arr[i];
-			}
-			
-			//print generated array
-			cout << "Generated array :" << endl;
-			for (int i = 0; i < size; i++) {
-				cout << arr[i] << " ";
-				if (i % 6 == 0 && i)
-					cout << endl;
-			}
 
-			cout << endl << endl << "Result : " << function(size, arr) << endl;
-			break;
+		if (N >= 0) {
+			cout << "Result : " << function(N);
+			return 0;
+		}
+		else {
+			cout << "error! program works with integer argument!" << endl;
+			return -1;
 		}
 	}
-	delete[] arr;
+
+	//cin-mode
+	cout << "Enter N : ";
+	try {
+		cin >> N;
+	}
+	catch (...) {
+		cout << "error! program works with integer argument!" << endl;
+		return -1;
+	}
+
+	cout << "Result : " << function(N);
 	return 0;
 }
